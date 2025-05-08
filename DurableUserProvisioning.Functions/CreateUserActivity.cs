fd57435327f44bb36a59205748f6851f3f6992e7
Blaise Liu <2326117+blaiseliu@users.noreply.github.com>
2025-05-08 12:22:57 -0400

    other functions

---
+++ DurableUserProvisioning.Functions/CreateUserActivity.cs
@@ -0,0 +1,14 @@
+﻿using DurableUserProvisioning.Models;
+using Microsoft.Azure.Functions.Worker;
+
+namespace DurableUserProvisioning.Functions;
+
+public static class CreateUserActivity
+{
+    [Function(nameof(CreateUserActivity))]
+    public static Task<string> Run([ActivityTrigger] User input)
+    {
+        // Replace this with real provisioning logic
+        return Task.FromResult($"User {input.Id} ({input.Email}) created.");
+    }
+}
---
+++ DurableUserProvisioning.Functions/ProvisionUserOrchestrator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DurableUserProvisioning.Models;
+
+namespace DurableUserProvisioning.Functions;
+public static class ProvisionUserOrchestrator
+{
+    [Function(nameof(ProvisionUserOrchestrator))]
+    public static async Task<string> Run(
+        [OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var userInfo = context.GetInput<User>();
+
+        string result = await context.CallActivityAsync<string>(
+            nameof(CreateUserActivity), userInfo);
+
+        return $"Provisioning complete: {result}";
+    }
+}
