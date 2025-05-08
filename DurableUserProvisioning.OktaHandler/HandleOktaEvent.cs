611c3fa4515bdd0ea2d2177660d777066f4c7f4e
Blaise Liu <2326117+blaiseliu@users.noreply.github.com>
2025-05-08 13:09:28 -0400

    handle okta

---
+++ DurableUserProvisioning.OktaHandler/HandleOktaEvent.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.EventGrid;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace DurableUserProvisioning.OktaHandler;
+public class HandleOktaEvent
+{
+    private readonly ILogger _logger;
+
+    public HandleOktaEvent(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<HandleOktaEvent>();
+    }
+
+    [Function("HandleOktaEvent")]
+    public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent)
+    {
+        _logger.LogInformation("Received event: {data}", eventGridEvent.Data.ToString());
+
+        // Parse event and act on it
+        var userEvent = JsonSerializer.Deserialize<UserProvisionedEvent>(eventGridEvent.Data.ToString());
+        _logger.LogInformation($"Provisioned user {userEvent?.Id} for Okta.");
+
+        // You can now call Okta APIs or log/audit
+        await Task.CompletedTask;
+    }
+}
---
+++ DurableUserProvisioning.OktaHandler/UserProvisionedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace DurableUserProvisioning.OktaHandler;
+
+public class UserProvisionedEvent
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+}
